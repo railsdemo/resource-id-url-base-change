30ad1dd8fd0ffd2a9b6b67022754fcda4dd7ff29
Bryan <bkapicka@gmail.com>
2013-07-26 10:42:44 -0400

    updated location model and controller

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,15 @@
 class Location < ActiveRecord::Base
+  
+  def to_param
+    AnyBase.encode(self.id, ::BASE28 )
+  end
+
+  def self.decode_id(enc_id)
+    find(AnyBase.decode(enc_id, ::BASE28 ))
+  end
+
+  def encoded_id
+    AnyBase.encode(self.id, ::BASE28 )
+  end
+  
 end
