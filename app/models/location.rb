99bda5a853c53188fb3a045e03061ccfbe464679
Bryan <bkapicka@gmail.com>
2013-07-27 20:52:18 -0400

    updated controller and simplified model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,15 +1,15 @@
 class Location < ActiveRecord::Base
-  
+
   def to_param
-    AnyBase.encode(self.id, ::BASE28 )
+    encoded_id
   end
 
-  def self.decode_id(enc_id)
-    find(AnyBase.decode(enc_id, ::BASE28 ))
+  def self.find(enc_id)
+    find_by_id(AnyBase.decode(enc_id, ::BASE28 ))
   end
 
   def encoded_id
-    AnyBase.encode(self.id, ::BASE28 )
+    AnyBase.encode(self[:id], ::BASE28 )
   end
-  
+
 end
